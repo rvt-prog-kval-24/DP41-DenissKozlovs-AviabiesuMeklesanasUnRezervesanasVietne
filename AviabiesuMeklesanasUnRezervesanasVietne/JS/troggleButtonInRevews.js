bf07a44d67c0616e1e82179d6bd9011f68d67d4b
Deniss Kozlovs DP4-1 <78017740+DenKozlov04@users.noreply.github.com>
2024-05-31 23:26:48 +0300

    Delete AviabiesuMeklesanasUnRezervesanasVietne/JS directory

--- AviabiesuMeklesanasUnRezervesanasVietne/JS/troggleButtonInRevews.js
+++
@@ -1,9 +0,0 @@
-function ShowEdit(commentId) {
-    var x = document.getElementById("editForm" + commentId);
-    if (x.style.display === "none" || x.style.display === "") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
-
